1e4737597d6ee2450d94a68d8022dbdb5ef471c3
李明 <44233423+xiaomingxiaomingdaxiaoming@users.noreply.github.com>
2020-10-16 17:18:16 +0800

    upload test02

---
+++ build/precache-manifest.361ae753666a54333d2940145385dbef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2ca2e68de354a3ce19981835a55442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb32afc0f3b5581dbb43",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "e6d081dd95ae96a27fcf",
+    "url": "/static/css/main.92a3ebff.chunk.css"
+  },
+  {
+    "revision": "cb32afc0f3b5581dbb43",
+    "url": "/static/js/2.c0bbbe0d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c0bbbe0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d081dd95ae96a27fcf",
+    "url": "/static/js/main.84c98483.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
